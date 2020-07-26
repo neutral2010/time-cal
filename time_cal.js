da1e63cefc9ae90390447084389274d486e8490d
吉本かおり <kasiwamoti5@gmail.com>
2020-07-26 18:23:07 +0900

    prepared files

---
+++ time_cal.js
@@ -0,0 +1,14 @@
+'user strict';
+
+document.getElementById('choice').textContent = new Date();
+console.log(document.getElementById('choice').textContent);
+
+// 勉強開始時、エンターキーが押されたら
+
+// その時刻を取得
+
+// 勉強終了時、エンターキーが押されたら、
+
+// その時間を取得
+
+// 先の時間と計算して、累計時間を計算し、表示
