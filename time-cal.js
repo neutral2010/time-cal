e576b7020a7b9e3815625f560ffdc632f6e9544b
吉本かおり <kasiwamoti5@gmail.com>
2020-08-23 11:42:08 +0900

    DotInstall-study

--- time-cal.js
+++ time-cal.js
@@ -1,34 +1,90 @@
-'user strict';
+'use strict';
 
-// 開始時刻を取得する
-var startTime = null;
-var displyArea = document.getElementById('display-area');
+{
+	// 要素を取得
+	const timer = document.getElementById('timer');
+	const start = document.getElementById('start');
+	const stop = document.getElementById('stop');
+	const reset = document.getElementById('reset');
 
-function start() {
-	startTime = Date.now();
-	console.log('スタートしました。')
-	document.body.onkeydown = stop;
-	console.log(startTime)
+	// 再代入される変数の定義（関数外でも使うもの）
+	let startTime;
+	let timeoutId; /* timeout用のId seTimeoutの戻り値として取得できる */
+	let elapsedTime = 0; /* タイマーが走っていた時間 */
+
+	// カウントアップの定義
+	function countUp () {
+		// 始まり（startTime）からの経過時間
+		// console.log(Date.now() - startTime);
+
+		// 時間表示を分かりやすくするためのDateオブジェクトを変数に代入
+		const d = new Date(Date.now() - startTime + elapsedTime);	
+		const h = String(d.getMinutes()).padStart(2, '0');
+		const m = String(d.getMinutes()).padStart(2, '0');
+		const s = String(d.getSeconds()).padStart(2, '0');
+		// const ms = String(d.getMilliseconds()).padStart(3, '0');
+		timer.textContent = `${h}:${m}:${s}`;
+
+		// 始まり（startTime）からの経過時間取得を10秒ごとに繰り返す
+		timeoutId = setTimeout(( ) => {
+			countUp();
+		}, 10);
+	}
+
+	// ボタンを最初の状態(スタートボタンのみ有効）にセットする関数
+	function setBottonStateInutual() {
+		start.classList.remove('inactive');
+		stop.classList.add('inactive');
+		reset.classList.add('inactive');
+	}
+
+	// ストップボタンのみ有効にセットする関数(classの追加・削除によって実装)
+	function setBottonStaterunning() {
+		start.classList.add('inactive');
+		stop.classList.remove('inactive');
+		reset.classList.add('inactive');
+	}
+
+	// スタートボタンとリセットボタンが有効にセットする関数
+	function setBottonStatestopped() {
+		start.classList.remove('inactive');
+		stop.classList.add('inactive');
+		reset.classList.remove('inactive');
+	}
+
+	// ページ読み込み時点で呼び出す
+	setBottonStateInutual();
+
+	// スタートボタンを押した時
+	start.addEventListener('click', () => {
+		if (start.classList.contains('inactive') === true) {
+			return;
+		}
+		setBottonStaterunning();
+		startTime = Date.now();
+		countUp();
+	});
+
+	// ストップボタンを押した時
+	stop.addEventListener('click', () => {
+		if (stop.classList.contains('inactive') === true) {
+			return;
+		}
+		setBottonStatestopped();
+		// 次のsetTimeoutがキャンセルになる
+		clearTimeout(timeoutId);
+		// ストップをクリックした時間はDate.now()で取得できる。
+		elapsedTime += Date.now( ) - startTime;	
+
+	});
+
+	// リセットボタンを押した時
+	reset.addEventListener('click', () => {
+		if (reset.classList.contains('inactive') === true) {
+			return;
+		}
+		setBottonStateInutual();
+		timer.textContent = '00:00:00';
+		elapsedTime = 0;
+	});
 }
-
-function stop() {
-	console.log('勉強終わりました。')
-// 終了時刻を取得
-	var stopTime = Date.now();
-	console.log (stopTime)
-	var sumTime = (stopTime - startTime) / 60000;
-	console.log(sumTime);
-	console.log (Math.round(sumTime)) //小数点以下を四捨五入
-
-// 結果を表示
-	displyArea.innerText = 'お疲れ様！今の勉強時間は、' + Math.round(sumTime) + '分でした。\nもう一度計測する場合は、ページを再読み込み（リロード）してください。';
-}
-if ( confirm('OKを押すと計測が始まります。')) {
-	start();
-}
-
-// ダイアログボックスじゃないようにしたい。
-// 継続して見れるようにしたい。
-
-
-
